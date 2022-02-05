e41dc280dbea37ae207709503ad9d8385336ce5c
“Aakash <aakash.1tha@gmail.com>
2022-02-05 19:23:36 +0545

    Co-authored-by: sashank dulal <Shawshank-dulal@users.noreply.github.com>

--- api/middleware/uploadImages.js
+++ api/middleware/uploadImages.js
@@ -1,17 +1,24 @@
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-// To store any kind of files
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // null is for error
-    cb(null, "./uploads/");
-  },
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-  filename: (req, file, cb) => {
-    // store current date-time as image name
-    cb(null, Date.now() + "_" + file.originalname.split(" ").join("_"));
+// Saving on Cloud Info
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "DEV",
+    public_id: (req, file) => {
+      return Date.now() + "_" + file.originalname.split(" ").join("_");
+    },
   },
 });
+
 const fileFilter = (req, file, cb) => {
   console.log(file);
   if (
@@ -31,7 +38,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   limits: { fileSize: 1024 * 1024 * 5 },
-  // fileFilter: fileFilter,
+  //fileFilter: fileFilter,
 });
 
 module.exports = upload.array("image", 8);
